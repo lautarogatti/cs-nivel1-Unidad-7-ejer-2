ff23fb60aa05b0c96c16c8e748d9b6a7875c5c8d
lautaro gatti <lautarogatti98@gmail.com>
2024-04-25 23:50:01 -0300

    subida al repositorio

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Hacer un programa que solicite 10  números enteros y los guarde en un vector. Luego recorrer ese vector para calcular el promedio. Mostrar por pantalla los valores que son mayores al promedio.*/
+
+            int n, acu = 0, prom;
+            int [] numeroh = new int [10];
+
+            for (int x = 0; x < 10; x++)
+            {
+                Console.WriteLine("Por favor ingrese un número: ");
+                n = int.Parse(Console.ReadLine());
+                numeroh[x] = n;
+                acu += numeroh[x];
+            }
+
+            prom = acu / 10;
+
+            Console.WriteLine("prom = " + prom);
+
+            for (int y = 0; y < 10; y++)
+            {
+                if (numeroh[y] > prom){
+                    Console.WriteLine(numeroh[y]);
+                }
+            }
+        }
+    }
+}
